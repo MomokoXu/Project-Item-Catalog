4090debe5564d3e2188845e1285149190a1ff74a
MomokoXu <momoko0808@hotmail.com>
2017-05-24 18:08:11 -0700

    add project database

---
+++ project/catalog/database_setup.py
@@ -0,0 +1,59 @@
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+# User object
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+# Catagory object
+class Catagory(Base):
+    __tablename__ = 'catagory'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+class Item(Base):
+    __tablename__ = 'item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    price = Column(String(8))
+    catagory_id = Column(Integer, ForeignKey('catagory.id'))
+    catagory = relationship(Catagory)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+# Serialize function to be able to send JSON objects in a
+# serializable format
+    @property
+    def serialize(self):
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id,
+            'price': self.price,
+            'catagory_id': self.catagory_id
+        }
+
+engine = create_engine('sqlite:///item_catalog.db')
+Base.metadata.create_all(engine)
